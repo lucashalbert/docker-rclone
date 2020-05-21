d05e00696a918c82f3e5365ba5d4602be78e1298
lucashalbert <lhalbert@lhalbert.xyz>
2020-05-21 18:00:34 +0000

    Initial s6-overlay testing

--- build-local.sh
+++ build-local.sh
@@ -4,14 +4,15 @@ build_date=${build_date:-$(date +"%Y%m%dT%H%M%S")}
 
 for docker_arch in amd64 arm32v6 arm64v8; do
     case ${docker_arch} in
-        amd64   ) qemu_arch="x86_64"  rclone_arch="amd64" ;;
-        arm32v6 ) qemu_arch="arm"     rclone_arch="arm"   ;;
-        arm64v8 ) qemu_arch="aarch64" rclone_arch="arm64" ;;    
+        amd64   ) qemu_arch="x86_64"  rclone_arch="amd64" s6_arch="amd64"   ;;
+        arm32v6 ) qemu_arch="arm"     rclone_arch="arm"   s6_arch="arm"     ;;
+        arm64v8 ) qemu_arch="aarch64" rclone_arch="arm64" s6_arch="aarch64" ;;    
     esac
     cp Dockerfile.cross Dockerfile.${docker_arch}
     sed -i "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__RCLONE_ARCH__|${rclone_arch}|g" Dockerfile.${docker_arch}
+    sed -i "s|__S6_ARCH__|${s6_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__RCLONE_VER__|${rclone_ver}|g" Dockerfile.${docker_arch}
     sed -i "s|__BUILD_DATE__|${build_date}|g" Dockerfile.${docker_arch}
     if [ ${docker_arch} == 'amd64' ]; then
@@ -32,10 +33,10 @@ for docker_arch in amd64 arm32v6 arm64v8; do
 
     # Build
     if [ "$EUID" -ne 0 ]; then
-        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver} .
-        #sudo docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}
+        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta .
+        #sudo docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
     else
-        docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver} .
-        #docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}
+        docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta .
+        #docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
     fi
 done
--- build.sh
+++ build.sh
@@ -4,14 +4,15 @@ build_date=${build_date:-$(date +"%Y%m%dT%H%M%S")}
 
 for docker_arch in amd64 arm32v6 arm64v8; do
     case ${docker_arch} in
-        amd64   ) qemu_arch="x86_64"  rclone_arch="amd64" image_arch="amd64" ;;
-        arm32v6 ) qemu_arch="arm"     rclone_arch="arm"   image_arch="arm"   ;;
-        arm64v8 ) qemu_arch="aarch64" rclone_arch="arm64" image_arch="arm64" ;;
+        amd64   ) qemu_arch="x86_64"  rclone_arch="amd64" image_arch="amd64" s6_arch="amd64"   ;;
+        arm32v6 ) qemu_arch="arm"     rclone_arch="arm"   image_arch="arm"   s6_arch="arm"     ;;
+        arm64v8 ) qemu_arch="aarch64" rclone_arch="arm64" image_arch="arm64" s6_arch="aarch64" ;;
     esac
     cp Dockerfile.cross Dockerfile.${docker_arch}
     sed -i "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__RCLONE_ARCH__|${rclone_arch}|g" Dockerfile.${docker_arch}
+    sed -i "s|__S6_ARCH__|${s6_arch}|g" Dockerfile.${docker_arch}
     sed -i "s|__RCLONE_VER__|${rclone_ver}|g" Dockerfile.${docker_arch}
     sed -i "s|__BUILD_DATE__|${build_date}|g" Dockerfile.${docker_arch}
     if [ ${docker_arch} == 'amd64' ]; then
@@ -32,26 +33,26 @@ for docker_arch in amd64 arm32v6 arm64v8; do
 
     # Build
     if [ "$EUID" -ne 0 ]; then
-        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver} .
-        sudo docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}
+        sudo docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta .
+        sudo docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
     else
         docker build -f Dockerfile.${docker_arch} -t lucashalbert/rclone:${docker_arch}-${rclone_var} .
-        docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}
+        docker push lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
 
         # Create and annotate arch-version manifest
-        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:${docker_arch}-${rclone_ver} lucashalbert/rclone:${docker_arch}-${rclone_ver}
-        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:${docker_arch}-${rclone_ver} lucashalbert/rclone:${docker_arch}-${rclone_ver} --os linux --arch ${image_arch}
-        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:${docker_arch}-${rclone_ver}
+        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
+        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta --os linux --arch ${image_arch}
+        DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta
     fi
 done
 
 
 
 # Create version specific docker manifest
-DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:${rclone_ver} lucashalbert/rclone:amd64-${rclone_ver} lucashalbert/rclone:arm32v6-${rclone_ver} lucashalbert/rclone:arm64v8-${rclone_ver}
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:${rclone_ver}-s6-beta lucashalbert/rclone:amd64-${rclone_ver}-s6-beta lucashalbert/rclone:arm32v6-${rclone_ver}-s6-beta lucashalbert/rclone:arm64v8-${rclone_ver}-s6-beta
 
 # Create latest docker manifest
-DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:latest lucashalbert/rclone:amd64-${rclone_ver} lucashalbert/rclone:arm32v6-${rclone_ver} lucashalbert/rclone:arm64v8-${rclone_ver}
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create lucashalbert/rclone:latest-s6-beta lucashalbert/rclone:amd64-${rclone_ver}-s6-beta lucashalbert/rclone:arm32v6-${rclone_ver}-s6-beta lucashalbert/rclone:arm64v8-${rclone_ver}-s6-beta
 
 for docker_arch in amd64 arm32v6 arm64v8; do
     case ${docker_arch} in
@@ -61,14 +62,14 @@ for docker_arch in amd64 arm32v6 arm64v8; do
     esac
 
     # Annotate version specific docker manifest
-    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:${rclone_ver} lucashalbert/rclone:${docker_arch}-${rclone_ver} --os linux --arch ${image_arch}
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:${rclone_ver}-s6-beta lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta --os linux --arch ${image_arch}
 
     # Annotate latest docker manifest
-    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:latest lucashalbert/rclone:${docker_arch}-${rclone_ver} --os linux --arch ${image_arch}
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest annotate lucashalbert/rclone:latest-s6-beta lucashalbert/rclone:${docker_arch}-${rclone_ver}-s6-beta --os linux --arch ${image_arch}
 done
 
 # Push version specific docker manifest
-DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:${rclone_ver}
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:${rclone_ver}-s6-beta
 
 # Push latest docker manifest
-DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:latest
+DOCKER_CLI_EXPERIMENTAL=enabled docker manifest push lucashalbert/rclone:latest-s6-beta
